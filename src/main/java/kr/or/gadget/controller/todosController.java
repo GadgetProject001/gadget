e5f7dadfe1d0f9d4c3de04711c8f0322eca95ec6
mxzo <mxzo@naver.com>
2023-02-03 13:21:01 +0900

    todo 기본구조

---
+++ src/main/java/kr/or/gadget/controller/todosController.java
@@ -0,0 +1,10 @@
+package kr.or.gadget.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/todos")
+public class todosController {
+
+}
---
+++ src/main/java/kr/or/gadget/dao/StateDao.java
@@ -0,0 +1,16 @@
+package kr.or.gadget.dao;
+
+import java.util.List;
+
+import kr.or.gadget.dto.State;
+
+public interface StateDao {
+	//state 생성
+	void insertState(State state);
+	//state 워크 스페이스 별 검색
+	List<State> selectBySpaceid(int spaceid);
+	//state 수정
+	int updateState(State state);
+	//state 삭제
+	int deleteState(int stateid);
+}
