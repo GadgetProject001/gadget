ec4e1013c9b5cf5596330159e5d3a6f43beaa78d
kimchansooo <kcs50604914@gmail.com>
2023-02-10 15:26:18 +0900

    2/10 채팅 화면 및 구현

--- src/main/java/kr/or/gadget/dao/StateDao.java
+++ src/main/java/kr/or/gadget/dao/StateDao.java
@@ -5,12 +5,12 @@ import java.util.List;
 import kr.or.gadget.dto.State;
 
 public interface StateDao {
-	//state 생성
-	void insertState(State state);
-	//state 워크 스페이스 별 검색
-	List<State> selectBySpaceid(int spaceid);
-	//state 수정
-	int updateState(State state);
-	//state 삭제
-	int deleteState(int stateid);
+//	//state 생성
+//	void insertState(State state);
+//	//state 워크 스페이스 별 검색
+//	List<State> selectBySpaceid(int spaceid);
+//	//state 수정
+//	int updateState(State state);
+//	//state 삭제
+//	int deleteState(int stateid);
 }
