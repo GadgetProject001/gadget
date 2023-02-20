7a7f33d0cace49a97967f3de27e9754ff0847237
kimchansooo <kcs50604914@gmail.com>
2023-02-20 17:03:30 +0900

    02/20 김찬수 채팅작업

--- src/main/java/kr/or/gadget/dao/ChatDao.java
+++ src/main/java/kr/or/gadget/dao/ChatDao.java
@@ -1,5 +1,16 @@
 package kr.or.gadget.dao;
 
-public interface ChatDao {
+import java.util.List;
 
+import kr.or.gadget.dto.Chat;
+
+public interface ChatDao {
+	
+	//특정 워크스페이스(채팅방)의 대화내용 가져오기
+	List<Chat> chatList(int spaceid);
+	
+	//대화내용 저장하기
+	int insertchat(Chat chat);
+	
+	
 }
